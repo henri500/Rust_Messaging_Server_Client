b10aa4bb64749c97aae34d098d51f20db6388123
CelineK <kocaslan.celine@gmail.com>
2021-10-17 19:17:38 +0200

    Add server

---
+++ server/target/debug/deps/server-d7913d7cdd497680.d
@@ -0,0 +1,5 @@
+/home/celine/Bureau/ESGI_LINUX/PSRL/Projet/Rust_Messaging_Server_Client/server/target/debug/deps/server-d7913d7cdd497680.rmeta: src/main.rs
+
+/home/celine/Bureau/ESGI_LINUX/PSRL/Projet/Rust_Messaging_Server_Client/server/target/debug/deps/server-d7913d7cdd497680.d: src/main.rs
+
+src/main.rs:
---
+++ server/target/rls/debug/deps/server-7051b6e50444f869.d
@@ -0,0 +1,5 @@
+/home/celine/Bureau/ESGI_LINUX/PSRL/Projet/Rust_Messaging_Server_Client/server/target/rls/debug/deps/server-7051b6e50444f869.rmeta: src/main.rs
+
+/home/celine/Bureau/ESGI_LINUX/PSRL/Projet/Rust_Messaging_Server_Client/server/target/rls/debug/deps/server-7051b6e50444f869.d: src/main.rs
+
+src/main.rs:
